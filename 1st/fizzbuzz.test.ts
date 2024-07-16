4f3bfdc63d828ae1efdf5b296ee6323a583a0252
chayutK <chayut.k@arise.tech>
2024-07-16 20:52:29 +0700

    testcase 14

--- 1st/fizzbuzz.test.ts
+++ 1st/fizzbuzz.test.ts
@@ -131,5 +131,15 @@ describe("Normal FizzBuzz",()=>{
 
       expect(actual).toEqual(expected)
     })
+
+    it("Given 14, should return '14'",()=>{
+      const given = 14
+
+      const expected = "14"
+
+      const actual = fizzBuzz(given)
+
+      expect(actual).toEqual(expected)
+    })
   })
 })
