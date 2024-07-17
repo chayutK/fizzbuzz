c8e704de3fa07b462121dd9de3e733f44d767e8e
chayutK <chayut.k@arise.tech>
2024-07-17 21:37:41 +0700

    testcase 9

--- 1st/fizzbuzz.test.ts
+++ 1st/fizzbuzz.test.ts
@@ -81,5 +81,15 @@ describe("one if fizzbuzz",()=>{
 
       expect(actual).toEqual(expected)
     })
+
+    it("Given 9, should return 'Fizz'",()=>{
+      const given = 9
+
+      const expected = "Fizz"
+
+      const actual = fizzBuzz(given)
+
+      expect(actual).toEqual(expected)
+    })
   })
 })
