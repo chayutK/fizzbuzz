786dcd777a127aac8b01d45dd98250cc518c61f1
chayutK <chayut.k@arise.tech>
2024-07-18 21:43:47 +0700

    testcase 5

--- 1st/fizzbuzz.ts
+++ 1st/fizzbuzz.ts
@@ -1,6 +1,10 @@
 export const fizzBuzz = (num:number) => {
-  if(num == 3){
-    return "Fizz"
+  const number = [5,3]
+  const result = ["Buzz","Fizz"]
+  for(var i = 0;i <number.length;i++){
+    if(num == number[i]){
+      return result[i]
+    }
   }
   return String(num)
 }
